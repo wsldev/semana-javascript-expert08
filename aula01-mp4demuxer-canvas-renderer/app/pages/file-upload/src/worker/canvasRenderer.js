8819dd8300e0248120d2a191e4ac4245e573ac62
Wagner dos Santos Lima <wslmacieira@gmail.com>
2023-10-27 13:36:58 -0300

    aula 02

---
+++ aula01-mp4demuxer-canvas-renderer/app/pages/file-upload/src/worker/canvasRenderer.js
@@ -0,0 +1,42 @@
+/** @param {HTMLCanvasElement} canvas */
+let _canvas = {}
+let _ctx = {}
+export default class CanvasRenderer {
+
+  /** @param {VideoFrame} frame  */
+  static draw(frame) {
+    const { displayHeight, displayWidth } = frame
+
+    _canvas.width = displayWidth
+    _canvas.height = displayHeight
+    _ctx.drawImage(
+      frame,
+      0,
+      0,
+      displayWidth,
+      displayHeight
+    )
+
+    frame.close()
+  }
+
+  static getRenderer(canvas) {
+    _canvas = canvas
+    _ctx = canvas.getContext('2d')
+    const renderer = this
+    let pendingFrame = null
+    return frame => {
+      const renderAnimationFrame = () => {
+        renderer.draw(pendingFrame)
+        pendingFrame = null
+      }
+      if (!pendingFrame) {
+        requestAnimationFrame(renderAnimationFrame)
+      } else {
+        pendingFrame.close()
+      }
+
+      pendingFrame = frame
+    }
+  }
+}
